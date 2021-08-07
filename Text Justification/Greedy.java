400e5f472ace4b5983ba11aa517275e1eb840298
AJW <84046974+AJ-Wuu@users.noreply.github.com>
2021-08-08 07:13:21 +0800

    Update Greedy.java

--- Text Justification/Greedy.java
+++ Text Justification/Greedy.java
@@ -12,67 +12,67 @@ public class Greedy {
 	//This code implements the greedy from the first word, and pads the right side with spaces until reaching the max width of the line.
 	
 	private static int findRight(int left, String[] strings, int limit) {
-        int right = left;
-        int sum = strings[right++].length();
+        	int right = left;
+        	int sum = strings[right++].length();
         
-        while (right < strings.length && (sum + 1 + strings[right].length()) <= limit) {
-            sum += 1 + strings[right++].length();
-        }
+        	while (right < strings.length && (sum + 1 + strings[right].length()) <= limit) {
+            		sum += 1 + strings[right++].length();
+        	}
             
-        return (right-1);
-    }
+        	return (right-1);
+    	}
 	
-    private static String blank(int length) {
-        return new String(new char[length]).replace('\0', ' ');
-    }
+    	private static String blank(int length) {
+        	return new String(new char[length]).replace('\0', ' ');
+    	}
     
-    private static String padResult(String result, int limit) {
-        return result + blank(limit - result.length());
-    }
+    	private static String padResult(String result, int limit) {
+        	return result + blank(limit - result.length());
+    	}
     
 	private static int stringsLength(int left, int right, String[] strings) {
-        int stringsLength = 0;
-        for (int i=left; i<=right; i++) {
-        	stringsLength += strings[i].length();
-        }
-        return stringsLength;
-    }
+        	int stringsLength = 0;
+        	for (int i=left; i<=right; i++) {
+        		stringsLength += strings[i].length();
+        	}
+        	return stringsLength;
+    	}
 
 	private static String justify(int left, int right, String[] strings, int limit) {
-        if (right - left == 0) {
-        	return padResult(strings[left], limit);
-        }
+        	if (right - left == 0) {
+        		return padResult(strings[left], limit);
+        	}
         
-        boolean isLastLine = (right == strings.length - 1);
-        int numSpaces = right - left;
-        int totalSpace = limit - stringsLength(left, right, strings);
+        	boolean isLastLine = (right == strings.length - 1);
+        	int numSpaces = right - left;
+        	int totalSpace = limit - stringsLength(left, right, strings);
         
-        String space = isLastLine ? " " : blank(totalSpace / numSpaces);
-        int remainder = isLastLine ? 0 : totalSpace % numSpaces;
+        	String space = isLastLine ? " " : blank(totalSpace / numSpaces);
+        	int remainder = isLastLine ? 0 : totalSpace % numSpaces;
         
-        StringBuilder result = new StringBuilder();
-        for (int i=left; i<=right; i++) {
-            result.append(strings[i])
-                  .append(space)
-                  .append(remainder-- > 0 ? " " : "");
-        }
+        	StringBuilder result = new StringBuilder();
+        	for (int i=left; i<=right; i++) {
+            		result.append(strings[i])
+                  	      .append(space)
+                  	      .append(remainder-- > 0 ? " " : "");
+        	}
         
-        return padResult(result.toString().trim(), limit);
-    }
+        	return padResult(result.toString().trim(), limit);
+    	}
 	
 	public static void textJustification(String[] strings, int limit) {
-        int left = 0;
-        ArrayList<String> result = new ArrayList<String>();
+        	int left = 0;
+        	ArrayList<String> result = new ArrayList<String>();
         
-        while (left < strings.length) {
-            int right = findRight(left, strings, limit);
-            result.add(justify(left, right, strings, limit));
-            left = right + 1;
-        }
+        	while (left < strings.length) {
+            		int right = findRight(left, strings, limit);
+            		result.add(justify(left, right, strings, limit));
+            		left = right + 1;
+        	}
         
-        for (int i=0; i<result.size(); i++) {
-        	System.out.println(result.get(i));
-        }
-    }
+        	for (int i=0; i<result.size(); i++) {
+        		System.out.println(result.get(i));
+        	}
+    	}
     
 }
