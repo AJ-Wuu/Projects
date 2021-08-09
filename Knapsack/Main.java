681457e06223a3c1a5f2a48c3b249bede2cf8f2c
AJW <84046974+AJ-Wuu@users.noreply.github.com>
2021-08-10 06:04:24 +0800

    Update Main.java

--- Knapsack/Main.java
+++ Knapsack/Main.java
@@ -1,1 +1,18 @@
-//
+/**
+ * @author AJWuu
+ */
+
+package knapsack;
+
+public class Main {
+
+	public static void main(String args[]) {
+		int val[] = new int[] { 60, 100, 120 };
+		int wt[] = new int[] { 10, 20, 30 };
+		int W = 50;
+		int n = val.length;
+		System.out.println(Recursive.knapSack(W, wt, val, n));
+		System.out.println(DP.knapSack(W, wt, val, n));
+	}
+
+}
