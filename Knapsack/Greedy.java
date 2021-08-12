32613ae2df7006a5b98dd9c747a0e771a0a2c53a
AJW <84046974+AJ-Wuu@users.noreply.github.com>
2021-08-13 03:02:51 +0800

    Update Greedy.java

--- Knapsack/Greedy.java
+++ Knapsack/Greedy.java
@@ -6,28 +6,20 @@ package knapsack;
 
 public class Greedy {
 
-	/* A Naive recursive implementation of 0-1 Knapsack problem */
-
-	// Returns the maximum value that can
-	// be put in a knapsack of capacity W
-	public static int knapSack(int W, int wt[], int val[], int n)
-	{
-		// Base Case
-		if (n == 0 || W == 0)
+	public static int knapSack(int W, int wt[], int val[], int n) {
+		//Base Case
+		if (n == 0 || W == 0) {
 			return 0;
+		}
 
-		// If weight of the nth item is more
-		// than Knapsack capacity W, then
-		// this item cannot be included in the optimal solution
-		if (wt[n - 1] > W)
-			return knapSack(W, wt, val, n - 1);
-
-		// Return the maximum of two cases:
-		// (1) nth item included
-		// (2) not included
-		else
-			return Math.max(val[n - 1] + knapSack(W - wt[n - 1], wt, val, n - 1),
-					knapSack(W, wt, val, n - 1));
+		//If weight of the nth item is more than Knapsack capacity W,
+		//then this item cannot be included in the optimal solution
+		if (wt[n-1] > W) {
+			return knapSack(W, wt, val, n-1);
+		}
+		else { //Return the maximum of two cases: nth item included OR not included
+			return Math.max(val[n-1] + knapSack(W - wt[n-1], wt, val, n-1), knapSack(W, wt, val, n-1));
+		}
 	}
 
 }
