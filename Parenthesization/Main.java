82b96c4403d748fb9e4d15ba3b1be3a3a2bc3da8
AJW <84046974+AJ-Wuu@users.noreply.github.com>
2021-08-13 02:32:02 +0800

    Update Main.java

--- Parenthesization/Main.java
+++ Parenthesization/Main.java
@@ -9,13 +9,14 @@ public class Main {
 	//There are 4 ways in total: ((T|T)&(F^T)), (T|(T&(F^T))), (((T|T)&F)^T) and (T|((T&F)^T))
 
 	public static void main(String[] args) {
-		char symbols[] = "TTFT".toCharArray();
-		char operators[] = "|&^".toCharArray();
+		String Symbols = "TTFT";
+        String Operators = "|&^";
+        
+		char symbols[] = Symbols.toCharArray();
+		char operators[] = Operators.toCharArray();
 		int n = symbols.length;
 		System.out.println(MemoCount.countParenthesis(symbols, operators, n));
 
-        String Symbols = "TTFT";
-        String Operators = "|&^";
         StringBuilder S = new StringBuilder();
         int j = 0;
         for (int i=0; i<Symbols.length(); i++){
