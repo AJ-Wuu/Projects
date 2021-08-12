ac9c36a87fcab37c7de33aecc7eed599eb6d462d
AJW <84046974+AJ-Wuu@users.noreply.github.com>
2021-08-13 03:05:00 +0800

    Update DP.java

--- Knapsack/DP.java
+++ Knapsack/DP.java
@@ -6,22 +6,21 @@ package knapsack;
 
 public class DP {
 
-		// Returns the maximum value that can be put in a knapsack
-		// of capacity W
-	public static int knapSack(int W, int wt[], int val[], int n)
-		{
-			int i, w;
-			int K[][] = new int[n + 1][W + 1];
+	public static int knapSack(int W, int wt[], int val[], int n) {
+			int K[][] = new int[n+1][W+1];
 
-			// Build table K[][] in bottom up manner
-			for (i = 0; i<= n; i++) {
-				for (w = 0; w<= W; w++) {
-					if (i == 0 || w == 0)
+			//Build table K[][] in Bottom-Up manner
+			for (int i=0; i<=n; i++) {
+				for (int w=0; w<=W; w++) {
+					if (i == 0 || w == 0) {
 						K[i][w] = 0;
-					else if (wt[i - 1]<= w)
-						K[i][w] = Math.max(val[i - 1] + K[i - 1][w - wt[i - 1]], K[i - 1][w]);
-					else
-						K[i][w] = K[i - 1][w];
+					}
+					else if (wt[i-1]<= w) {
+						K[i][w] = Math.max(val[i-1] + K[i-1][w - wt[i-1]], K[i-1][w]);
+					}
+					else {
+						K[i][w] = K[i-1][w];
+					}
 				}
 			}
 
