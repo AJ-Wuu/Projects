db15b99d4f1cf088cc468e8ed5a9b8acbe0edb78
AJW <84046974+AJ-Wuu@users.noreply.github.com>
2021-08-16 02:50:21 +0800

    Create Rank.java

---
+++ Blackjack/Perfect-Information Blackjack/Rank.java
@@ -0,0 +1,22 @@
+/**
+ * @author AJWuu
+ */
+
+package blackjack;
+
+public enum Rank {
+	//There is no Joker here
+	Two,
+	Three,
+	Four,
+	Five,
+	Six,
+	Seven,
+	Eight,
+	Nine,
+	Ten,
+	Jack,
+	Queen,
+	King,
+	Ace;
+}
