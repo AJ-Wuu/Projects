8315ff1968c4167975f27665258a9ac50c675baf
AJW <84046974+AJ-Wuu@users.noreply.github.com>
2021-08-19 02:13:21 +0800

    Update Driver.java

--- Tetris/Driver.java
+++ Tetris/Driver.java
@@ -1,1 +1,19 @@
+/**
+ * @author AJWuu
+ */
 
+package tetris;
+
+import java.awt.EventQueue;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(() -> {
+			Frame game = new Frame();
+			game.startUI();
+		});
+
+	}
+
+}
