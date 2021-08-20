67d4f57a228e5c6a0a70dec761f4ef4b1bcbe19c
AJW <84046974+AJ-Wuu@users.noreply.github.com>
2021-08-21 07:53:51 +0800

    Update Board.java

--- Tetris/Board.java
+++ Tetris/Board.java
@@ -4,169 +4,135 @@
 
 package tetris;
 
-import java.awt.Color;
+import java.awt.BasicStroke;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.geom.Area;
+import java.awt.geom.Rectangle2D;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
-public class Board {
+@SuppressWarnings("serial")
+public class Board extends JPanel implements ActionListener, KeyListener {
 
-	private static final int squareSize = 20;
-	private static int boardWidth, boardHeight; //the number of squares for the board's width and height
-	public static JPanel board;
-	private static JLabel status;
+	private static int squareSize, boardWidth, boardHeight, squareNumber, charRow, charCol;
+	private static Frame frame;
+	private static JPanel board;
+	private static boolean roundFinished = true;
 
-	public Board(Frame frame) {
-		board = new JPanel(null);
-		//1. change the Layout in the parenthesis (to BorderLayout, for example) for better arranging the place of buttons
-		//   Note that the locations would be strictly aligned to the layout defaults, and setBounds() would not work
-		//2. change the layout to null will make the panel in the "Absolute Layout",
-		//   where it's able to set the position of the JLabel and JButton with setBounds()
-		status = frame.status;
+	Timer timer = new Timer(5, this);
+	int x, y, velX = 0, velY = 1, x_right = 0, y_right = 0;
+	int[] xPoints, yPoints;
+	ShapeCharWithColor shape;
+	int degrees;
+	char[] currShapeArray;
+	Area shapeG2 = new Area();
+
+	public Board(Frame jframe) {
+		frame = jframe;
 		boardWidth = frame.Width;
-		boardHeight = frame.Height;
-		freshBoard(squareSize);
+		boardHeight = frame.Height - 3*frame.labelSize - frame.buttonSize;
+		squareSize = frame.squareSize;
+		squareNumber = PieceShapeInChar.pieceNumber;
+		charRow = PieceShapeInChar.pieceWidth;
+		charCol = PieceShapeInChar.pieceHeight;
+
+		x = boardWidth/2 - squareSize;
+		y = 2;
+		xPoints = new int[4 * squareNumber];
+		yPoints = new int[4 * squareNumber];
+
+		board = this;
+		this.setSize(new Dimension(boardWidth, boardHeight));
+		this.setBounds(0, jframe.buttonSize, boardWidth, boardHeight);
+		
+		timer.start();
+		this.addKeyListener(this);
+		this.setFocusable(true); //allow to use a KeyListener
+		//Focus request will only be possibly granted after components have been rendered, so neither pack() nor setVisible(true) should be called before this
+		//TAB controls the switch between buttons and KeyListener (in other words, changes the "focus" of the program)
+		//To use the KeyListener, we need to press TAB & arrow together
+		this.setFocusTraversalKeysEnabled(false); //avoid bizarre actions, like "Tab" key
+	}
+	
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		Graphics2D g2 = (Graphics2D) g;
+		g2.setStroke(new BasicStroke(3));
+		g2.drawLine(0, squareSize*squareNumber, boardWidth, squareSize*squareNumber);
+//		while (roundFinished) {
+			shape = ShapeCharWithColor.J; //PieceShapeInChar.getRandomShape();
+			shapeG2 = buildShape(shape);
+			g2.setColor(shape.getColor());
+//			stop(g2);		
+//		}	
+		g2.fill(shapeG2);
 	}
 
-	private void freshBoard(int squareSize) {
-		board.setPreferredSize(new Dimension(boardWidth, boardHeight));
-		board.setBackground(Color.white);
-		board.add(new Buttons(board, status, boardWidth, boardHeight));
-		board.add(new KeyMoves(board, squareSize, boardWidth/2));  
+	private Area buildShape(ShapeCharWithColor shape) {
+		int[] shapeLocation = getShape(shape);
+		Area fin = new Area(); //used to combine several shapes together
+		int paraX = shapeLocation[0], paraY = shapeLocation[1];
+		int diffX, diffY;
+		for (int i=0; i<squareNumber; i++) {
+			diffX = (shapeLocation[i*2] - paraX) * squareSize;
+			diffY = (shapeLocation[i*2+1] - paraY) * squareSize;
+			Area temp = new Area(new Rectangle2D.Double(x+diffX, y+diffY, squareSize, squareSize));
+			fin.add(temp);
+			x_right = Math.max(x+diffX, x_right);
+			y_right = Math.max(y+diffY, y_right);
+		}
+		return fin;
 	}
 
-	public static void start() {
-		System.out.println("Start");
-//		Tiles tiles = new Tiles(squareSize, boardWidth, boardHeight, (int)(boardHeight/squareSize), (int)(boardWidth/squareSize), board);
+	private int[] getShape(ShapeCharWithColor shape) {
+		degrees = 0;//PieceShapeInChar.getRandomDegrees();
+		currShapeArray = PieceShapeInChar.rotateShapes(shape.getShape(), degrees);
+		return getShapeHelper(currShapeArray);
 	}
 
-	public static void pause() {
-		try {
-			Thread.sleep(5);
-		} catch (InterruptedException except) {
-			except.printStackTrace();
+	private int[] getShapeHelper(char[] currShapeArray) {
+		int[] location = new int[2*squareNumber];
+		for (int i=0, j=0; i<charRow*charCol; i++) {
+			if (currShapeArray[i] == 'X') {
+				location[j] = i % 4;
+				location[j+1] = i / 4;
+				j += 2;
+			}
+		}
+		return location;
+	}
+
+	private void stop(Graphics2D g2) {
+		roundFinished = false;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		repaint();
+		x += velX;
+		y += velY;
+	}
+	
+	@Override
+	public void keyPressed(KeyEvent e) {
+		int code = e.getKeyCode(); //each key has its own unique code
+		if (code == KeyEvent.VK_LEFT) {
+			velX = -1;
+		}
+		else if (code == KeyEvent.VK_RIGHT) {
+			velX = 1;
 		}
 	}
 
-	public static void stop() {
-		Buttons.pauseButton.setEnabled(false);
-		try {
-			Thread.sleep(5);
-		} catch (InterruptedException except) {
-			except.printStackTrace();
-		}
-		status.setText("You've earned: " + String.valueOf(Tiles.numLinesRemoved));
-	}
-
-
-	//	private Point[] getNewPiece() {
-	//		ShapePointWithColor shape = PieceShapeInPoint.getRandomShape();
-	//		Point[] piece = PieceShapeInPoint.rotateShapes(shape, PieceShapeInPoint.getRandomDegrees());
-	//		int[] XYlength = PieceShapeInPoint.XYlength(piece);
-	//		posX = (numSquareWidth - XYlength[0]) / 2; //always start from the middle
-	//		posY = numSquareHeight - 1 + XYlength[1];
-	//
-	//		if (!moveAction(piece, posX, posY, XYlength)) {
-	//			piece = blankPiece;
-	//			timer.stop();
-	//			String message = String.format("Game over. Score: %d", numLinesRemoved);
-	//			status.setText(message);
-	//		}
-	//		return piece;
-	//	}
-	//
-	//	private boolean moveAction(Point[] newPiece, int newX, int newY, int[] XYlength) {
-	//		for (int i=0; i<shapeSize; i++) {
-	//			int x = newX + XYlength[0];
-	//			int y = newY - XYlength[1];
-	//			if (x < 0 || x >= numSquareWidth || y < 0 || y >= numSquareHeight) {
-	//				return false;
-	//			}
-	//		}
-	//		currPiece = newPiece;
-	//		posX = newX;
-	//		posY = newY;
-	//		repaint(); //Calling JPanel.repaint() will have all the component within the JPanel to repaint()
-	//		return true;
-	//	}
-	//
-	//	private class GameCycle implements ActionListener {
-	//		@Override
-	//		public void actionPerformed(ActionEvent event) {
-	//			runGameCycle();
-	//		}
-	//	}
-	//
-	//	private void runGameCycle() {
-	//		update();
-	//		repaint();
-	//	}
-	//
-	//	private void update() {
-	//		if (isPaused) {
-	//			return ;
-	//		}
-	//		if (isFinished) {
-	//			isFinished = false;
-	//			currPiece = getNewPiece();
-	//		}
-	//		else {
-	//			oneLineDown();
-	//		}
-	//	}
-	//
-	//	private void oneLineDown() {
-	//		int[] XYlength = PieceShapeInPoint.XYlength(currPiece);
-	//		if (!moveAction(currPiece, posX, posY - 1, XYlength)) {
-	//			pieceDropped(XYlength);
-	//		}
-	//	}
-	//
-	//	private void pieceDropped(int[] XYlength) {
-	//		removeFullLines();
-	//		if (!isFinished) {
-	//			currPiece = getNewPiece();
-	//		}
-	//	}
-	//
-	//	private void removeFullLines() {
-	//		int numFullLines = 0;
-	//		for (int i=numSquareHeight-1; i>=0; i--) { //from bottom to top
-	//			boolean isFullLine = true;
-	//			for (int j=0; j<numSquareWidth; j++) { //from left to right
-	//				if (shapeAt(j, i) == blankPiece) {
-	//					isFullLine = false;
-	//					break;
-	//				}
-	//			}
-	//
-	//			if (isFullLine) {
-	//				numFullLines++;
-	//				for (int k = i; k < boardHeight - 1; k++) {
-	//					for (int j = 0; j < boardWidth; j++) {
-	//						board[(k * boardWidth) + j] = shapeAt(j, k + 1);
-	//					}
-	//				}
-	//			}
-	//		}
-	//
-	//		if (numFullLines > 0) {
-	//			numLinesRemoved += numFullLines;
-	//			status.setText(String.valueOf(numLinesRemoved));
-	//			isFinished = true;
-	//			currPiece.setShape(blankPiece);
-	//		}
-	//	}
-	//
-	//	private void Pause() {
-	//		if (isPaused) {
-	//			status.setText("Paused");
-	//		}
-	//		else {
-	//			status.setText(String.valueOf(numLinesRemoved));
-	//		}
-	//		repaint();
-	//	}
+	public void keyReleased(KeyEvent e) {}
+	public void keyTyped(KeyEvent e) {}
 
 }
