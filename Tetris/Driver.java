6878cfb725e9e5046e36ca81b6ad0010a914187d
AJW <84046974+AJ-Wuu@users.noreply.github.com>
2021-08-22 23:28:59 +0800

    Update Driver.java

--- Tetris/Driver.java
+++ Tetris/Driver.java
@@ -17,6 +17,10 @@ public class Driver {
 			EventQueue.invokeLater(() -> {
 				@SuppressWarnings("unused")
 				Frame game = new Frame(scan, 0);
+				//First, There is absolutely no need to close the System.in stream.
+				//	Other parts of the program may be using it, and you don't want to interfere with their normal operation.
+				//Second, there's no benefit to creating more than one Scanner object.
+				//	It's simply reading input from a stream, and having more than one reference isn't necessary or beneficial to your operations.
 			});
 		}
 		else {
