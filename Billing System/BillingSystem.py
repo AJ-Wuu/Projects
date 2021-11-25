21371915221271482e6c3faa1f2296b4c520a144
JayW <84046974+AJ-Wuu@users.noreply.github.com>
2021-11-25 00:15:12 -0600

    Update BillingSystem.py

--- Billing System/BillingSystem.py
+++ Billing System/BillingSystem.py
@@ -3,13 +3,23 @@
 import json
 from python_freeipa import ClientMeta
 
-with open ('customers.json','r') as jsonFile:
-    customers = json.load(jsonFile)
+json_file = 'customers.json'
+with open(json_file, 'r') as jsonFile:
+    records = json.loads(jsonFile.read())
 
 client = ClientMeta('ipa.demo1.freeipa.org')
+#client = ClientMeta(dns_lookup=True)
 client.login('admin', 'Secret123')
-user = client.user_add('test3', 'John', 'Doe', 'John Doe', o_preferredlanguage='EN')
-user = client.user_mod()
 
-group = client.group_add()
-group = client.group_mod()
+userAdd = client.user_add('test3', 'John', 'Doe',
+                          'John Doe', o_preferredlanguage='EN')
+
+userMod = client.user_mod()
+
+userDel = client.user_del()
+
+groupAdd = client.group_add()
+
+groupMod = client.group_mod()
+
+groupDel = client.group_del()
