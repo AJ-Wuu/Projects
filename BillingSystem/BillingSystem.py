8ff6935072d99295e7c9bb45368a722538250ae0
AJWuu <84046974+AJ-Wuu@users.noreply.github.com>
2022-01-03 16:18:00 -0600

    Update and rename Billing System/BillingSystem.py to BillingSystem/BillingSystem.py

--- BillingSystem/BillingSystem.py
+++ BillingSystem/BillingSystem.py
@@ -1,11 +1,25 @@
-'''@author AJWuu'''
-
 import json
 from python_freeipa import ClientMeta
 
 json_file = 'customers.json'
 with open(json_file, 'r') as jsonFile:
     records = json.loads(jsonFile.read())
+
+
+class customer:
+    Department = 'unknown department'
+    Email = 'xxx@xxx.edu'
+    FirstName = 'firstname'
+    LastName = 'lastname'
+    Name = 'firstname lastname'
+
+    def __init__(self, Department, Email, FirstName, LastName):
+        self.Department = Department
+        self.Email = Email
+        self.FirstName = FirstName
+        self.LastName = LastName
+        self.Name = FirstName + LastName
+
 
 client = ClientMeta('ipa.demo1.freeipa.org')
 #client = ClientMeta(dns_lookup=True)
@@ -23,3 +37,5 @@ groupAdd = client.group_add()
 groupMod = client.group_mod()
 
 groupDel = client.group_del()
+
+print(userAdd)
