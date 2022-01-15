6aed48fe9dea27e8d8146fad97a84b653b5bf90c
AJWuu <84046974+AJ-Wuu@users.noreply.github.com>
2022-01-14 20:53:03 -0600

    Update Regression_NonLinearRegression_GDP.py

--- Machine Learning/PracticeTraining/Regression_NonLinearRegression_GDP.py
+++ Machine Learning/PracticeTraining/Regression_NonLinearRegression_GDP.py
@@ -34,7 +34,7 @@ def train(df, xdata, ydata):
 
 def main():
     # data source: !wget - nv - O china_gdp.csv https: // cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module % 202/data/china_gdp.csv
-    df = pd.read_csv("china_gdp.csv")
+    df = pd.read_csv("ChinaGDP.csv")
 
     # plot the data
     plt.figure(figsize=(8, 5))
