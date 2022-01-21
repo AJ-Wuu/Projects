b6d1721a6e5d54d9f898ac625bf13f4ddba96938
AJWuu <84046974+AJ-Wuu@users.noreply.github.com>
2022-01-21 12:32:56 -0600

    Delete Blockchain/BigchainDB/App/src directory

--- Blockchain/BigchainDB/App/src/bigchaindb/Connection.tsx
+++
@@ -1,6 +0,0 @@
-import * as BigchainDB from 'bigchaindb-driver';
-
-const API_PATH = 'https://test.ipdb.io/api/v1/'
-const BigchainDBConnection = new BigchainDB.Connection(API_PATH);
-
-export default BigchainDBConnection;
